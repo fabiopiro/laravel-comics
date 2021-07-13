62c88392e886c638812379f8eac2789f25849992
fabiopiro <piroddifabio01@gmail.com>
2021-07-13 15:19:51 +0200

    header base

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        {{-- CSS --}}
+        {{-- asset() punta direttamente alla cartella 'public' --}}
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        {{-- CSS --}}
+        <title>Document</title>
+    </head>
+    <body>
+        
+
+        <header>
+            <div class="header-top">
+                
+            </div>
+            <nav>
+                <img src="{{ asset('img/dc-logo.png') }}" alt="dc logo">
+            </nav>
+            <div class="jumbotron">
+
+            </div>
+        </header>
+
+        <main></main>
+
+        <footer></footer>
+
+
+    </body>
+</html>
