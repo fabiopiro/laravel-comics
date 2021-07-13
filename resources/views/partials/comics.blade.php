a371702d22d773dde52f8877613ca9b06277828c
fabiopiro <piroddifabio01@gmail.com>
2021-07-13 17:33:34 +0200

    comics & header css

--- resources/views/partials/comics.blade.php
+++ resources/views/partials/comics.blade.php
@@ -1,10 +1,12 @@
-<div class="container">
-    @foreach ($comicsArray as $comic)
-        <div class="card">
-            <img src="{{ $comic['thumb']}}" alt="{{ $comic['title']}}">
-            <p>{{ $comic['title']}}</p>
-        </div>  
-    @endforeach
+<div class="comics">
+    <div class="container width-75">
+        @foreach ($comicsArray as $comic)
+            <div class="card">
+                <img src="{{ $comic['thumb']}}" alt="{{ $comic['title']}}">
+                <p>{{ $comic['title']}}</p>
+            </div>  
+        @endforeach
+    </div>
 </div>
 {{-- @dump($comicsArray) --}}
 <nav class="comics-nav">
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,8 +2,42 @@
     <div class="header-top">
 
     </div>
-    <nav>
+    <nav class="header-nav width-75">
         <img src="{{ asset('img/dc-logo.png') }}" alt="dc logo">
+        <ul class="list-inline">
+            <li>
+                <a href="">characters</a>
+            </li>
+            <li>
+                <a href="">comics</a>
+            </li>
+            <li>
+                <a href="">movies</a>
+            </li>
+            <li>
+                <a href="">tv</a>
+            </li>
+            <li>
+                <a href="">games</a>
+            </li>
+            <li>
+                <a href="">collectibles</a>
+            </li>
+            <li>
+                <a href="">videos</a>
+            </li>
+            <li>
+                <a href="">fans</a>
+            </li>
+            <li>
+                <a href="">news</a>
+            </li>
+            <li>
+                <a href="">shop</a>
+            </li>
+            
+
+        </ul>
     </nav>
     {{-- opzione background dinamico --}}
     {{-- <div class="jumbotron" style="background-image: url('{{ assets("img/jumbotron.jpg") }}')"> --}}
